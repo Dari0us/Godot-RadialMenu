e77fcfc2fee198ae392a65fec53f21c33058ab4a
Dari0us <d4ri0u5@gmail.com>
2025-06-06 18:54:22 +0300

    MetaSource Fundamental Rework
    The first iteration of the metasource conscept was fundamentally flawed and limited. this is phase 2, addressing these limitations.

--- addons/RadialMenu/Scripts/RadialMenuBus.gd
+++ addons/RadialMenu/Scripts/RadialMenuBus.gd
@@ -8,62 +8,157 @@ extends Node
 # IN THIS CASE I JUST PUT EVERYTHING IN THE GENERAL FOLDER AND REFERENCE THEM DIRECTLY
 const ICON_PATH = "res://addons/RadialMenu/ICONS/GENERAL/"
 
-var menu_construct_example: Dictionary = { 
-	"meta_source": null,
-	"menu_construct": {
-		"[EDITOR EXAMPLE CATEGORY 0]": {
-			"icon": ICON_PATH+"UI_TESTICON.png",
-			"description": "Check The Documentation In The RadialMenu Folder",
-			"command": "example_command",
-			"meta_input": null,
-		},
-		"[EDITOR EXAMPLE CATEGORY 1]": {
-			"icon": ICON_PATH+"UI_TESTICON.png",
-			"description": "Check The Documentation In The RadialMenu Folder",
-			"command": "example_command",
-			"meta_input": null,
-			"sub_items": {
-				"[EDITOR EXAMPLE ActionA]": {
-					"icon": ICON_PATH+"UI_TESTICON2.png",
-					"description": "Check The Documentation In The RadialMenu Folder",
-					"command": "example_command",
-					"meta_input": null,
-				},
-				"[EDITOR EXAMPLE ActionB]": {
-					"icon": ICON_PATH+"UI_TESTICON2.png",
-					"description": "Check The Documentation In The RadialMenu Folder",
-					"command": "example_command",
-					"meta_input": null,
-				},
-				"[EDITOR EXAMPLE ActionC]": {
-					"icon": ICON_PATH+"UI_TESTICON2.png",
-					"description": "Check The Documentation In The RadialMenu Folder",
-					"command": "example_command",
-					"meta_input": null,
-				}
+var test_source : Dictionary = {
+	"apple":{
+		"hp": 5,
+		"potassium n sht": "an apples worth"
+	},
+	"banana":{
+		"hp": 10,
+		"potassium n sht": "a lot"
+	},
+}
+
+var menu_construct_test: Dictionary = {
+	"Category0": {
+		"icon": ICON_PATH+"UI_TESTICON.png",
+		"description": "Run a quick action.",
+		"command": "immediate_category", ##SCROLL DOWN TO SEE THE EXAMPLE FUNCTION
+		"meta_source": null,
+		"meta_input": null,
+	},
+	"Category1": {
+		"icon": ICON_PATH+"UI_TESTICON.png",
+		"description": "Camp and survival actions.",
+		"command": "select_category_1",
+		"meta_source": null,
+		"meta_input": null,
+		"sub_items": {
+			"ActionA": {
+				"icon": ICON_PATH+"UI_TESTICON2.png",
+				"description": "Meta Source Proof of conscept Apple.",
+				"command": "meta_source_test",
+				"meta_source": test_source,
+				"meta_input": 0,
+			},
+			"ActionB": {
+				"icon": ICON_PATH+"ICON_CAMPFIRE.png",
+				"description": "Set up a campfire to rest and cook.",
+				"command": "create_campfire",
+				"meta_source": null,
+				"meta_input": null,
+			},
+			"ActionC": {
+				"icon": ICON_PATH+"UI_TESTICON2.png",
+				"description": "Meta Source Proof of conscept Banana.",
+				"command": "meta_source_test",
+				"meta_source": test_source,
+				"meta_input": 1,
 			}
-		},
-		"[EDITOR EXAMPLE CATEGORY 2]": {
-			"icon": ICON_PATH+"UI_TESTICON.png",
-			"description": "Check The Documentation In The RadialMenu Folder",
-			"command": "example_command",
-			"meta_input": null,
-			"sub_items": {
-				"[EDITOR EXAMPLE ActionD]": {
-					"icon": ICON_PATH+"UI_TESTICON2.png",
-					"description": "Check The Documentation In The RadialMenu Folder",
-					"command": "example_command",
-					"meta_input": null,
-				},
-				"[EDITOR EXAMPLE ActionE]": {
-					"icon": ICON_PATH+"UI_TESTICON2.png",
-					"description": "Check The Documentation In The RadialMenu Folder",
-					"command": "example_command",
-					"meta_input": null,
-				},
-			}
-		},
+		}
+	},
+	"Category2": {
+		"icon": ICON_PATH+"UI_TESTICON.png",
+		"description": "Resource or crafting actions.",
+		"command": "select_category_2",
+		"meta_source": null,
+		"meta_input": null,
+		"sub_items": {
+			"ActionD": {
+				"icon": ICON_PATH+"ICON_CAMPFIRE.png",
+				"description": "Resource gathering action.",
+				"command": "bus_test_two",
+				"meta_source": null,
+				"meta_input": null,
+			},
+			"ActionE": {
+				"icon": ICON_PATH+"ICON_CAMPFIRE.png",
+				"description": "Craft a useful tool.",
+				"command": "bus_test_two",
+				"meta_source": null,
+				"meta_input": null,
+			},
+		}
+	},
+	"Category3": {
+		"icon": ICON_PATH+"UI_TESTICON.png",
+		"description": "Advanced actions.",
+		"command": "select_category_3",
+		"meta_source": null,
+		"meta_input": null,
+		"sub_items": {
+			"ActionF": {
+				"icon": ICON_PATH+"ICON_CAMPFIRE.png",
+				"description": "Special advanced action.",
+				"command": "bus_test_two",
+				"meta_source": null,
+				"meta_input": null,
+			},
+		}
 	}
+}
+
+var menu_construct_example: Dictionary = { 
+	"[EDITOR EXAMPLE CATEGORY 0]": {
+		"icon": ICON_PATH+"UI_TESTICON.png",
+		"description": "Check The Documentation In The RadialMenu Folder",
+		"command": "example_command",
+		"meta_source": null,
+		"meta_input": null,
+	},
+	"[EDITOR EXAMPLE CATEGORY 1]": {
+		"icon": ICON_PATH+"UI_TESTICON.png",
+		"description": "Check The Documentation In The RadialMenu Folder",
+		"command": "example_command",
+		"meta_source": null,
+		"meta_input": null,
+		"sub_items": {
+			"[EDITOR EXAMPLE ActionA]": {
+				"icon": ICON_PATH+"UI_TESTICON2.png",
+				"description": "Check The Documentation In The RadialMenu Folder",
+				"command": "example_command",
+				"meta_source": null,
+				"meta_input": null,
+			},
+			"[EDITOR EXAMPLE ActionB]": {
+				"icon": ICON_PATH+"UI_TESTICON2.png",
+				"description": "Check The Documentation In The RadialMenu Folder",
+				"command": "example_command",
+				"meta_source": null,
+				"meta_input": null,
+			},
+			"[EDITOR EXAMPLE ActionC]": {
+				"icon": ICON_PATH+"UI_TESTICON2.png",
+				"description": "Check The Documentation In The RadialMenu Folder",
+				"command": "example_command",
+				"meta_source": null,
+				"meta_input": null,
+			}
+		}
+	},
+	"[EDITOR EXAMPLE CATEGORY 2]": {
+		"icon": ICON_PATH+"UI_TESTICON.png",
+		"description": "Check The Documentation In The RadialMenu Folder",
+		"command": "example_command",
+		"meta_source": null,
+		"meta_input": null,
+		"sub_items": {
+			"[EDITOR EXAMPLE ActionD]": {
+				"icon": ICON_PATH+"UI_TESTICON2.png",
+				"description": "Check The Documentation In The RadialMenu Folder",
+				"command": "example_command",
+				"meta_source": null,
+				"meta_input": null,
+			},
+			"[EDITOR EXAMPLE ActionE]": {
+				"icon": ICON_PATH+"UI_TESTICON2.png",
+				"description": "Check The Documentation In The RadialMenu Folder",
+				"command": "example_command",
+				"meta_source": null,
+				"meta_input": null,
+			},
+		}
+	},
 }
 
 ##YOU CAN ADD AS MANY OF THESE DICTIONARIES HERE
@@ -71,79 +166,15 @@ var menu_construct_example: Dictionary = {
 ## THIS ENTIRE SCRIPT BASICALLY ACTS AS A HOLDER FOR THE DIFFERENT CONSTRUCTS
 
 
-var menu_construct_test: Dictionary = {
-	"meta_source": null,
-	"menu_construct": {
-		"Category0": {
-			"icon": ICON_PATH+"UI_TESTICON.png",
-			"description": "Run a quick action.",
-			"command": "immediate_category",
-			"meta_input": null,
-		},
-		"Category1": {
-			"icon": ICON_PATH+"UI_TESTICON.png",
-			"description": "Camp and survival actions.",
-			"command": "select_category_1",
-			"meta_input": null,
-			"sub_items": {
-				"ActionA": {
-					"icon": ICON_PATH+"ICON_CAMPFIRE.png",
-					"description": "This is a test action.",
-					"command": "bus_test_one",
-					"meta_input": null,
-				},
-				"ActionB": {
-					"icon": ICON_PATH+"ICON_CAMPFIRE.png",
-					"description": "Set up a campfire to rest and cook.",
-					"command": "create_campfire",
-					"meta_input": null,
-				},
-				"METASOURCE TEST": {
-					"icon": ICON_PATH+"UI_TESTICON2.png",
-					"description": "TEST METASOURCE OUTPUT",
-					"command": "bus_test_two",
-					"meta_input": 1, #in this case, the id of the item
-				}
-			}
-		},
-		"Category2": {
-			"icon": ICON_PATH+"UI_TESTICON.png",
-			"description": "Resource or crafting actions.",
-			"command": "select_category_2",
-			"meta_input": null,
-			"sub_items": {
-				"ActionD": {
-					"icon": ICON_PATH+"ICON_CAMPFIRE.png",
-					"description": "Resource gathering action.",
-					"command": "bus_test_two",
-					"meta_input": null,
-				},
-				"ActionE": {
-					"icon": ICON_PATH+"ICON_CAMPFIRE.png",
-					"description": "Craft a useful tool.",
-					"command": "bus_test_two",
-					"meta_input": null,
-				},
-			}
-		},
-		"Category3": {
-			"icon": ICON_PATH+"UI_TESTICON.png",
-			"description": "Advanced actions.",
-			"command": "select_category_3",
-			"meta_input": null,
-			"sub_items": {
-				"ActionF": {
-					"icon": ICON_PATH+"ICON_CAMPFIRE.png",
-					"description": "Special advanced action.",
-					"command": "bus_test_two",
-					"meta_input": null,
-				},
-			}
-		}
-	}
-}
+
+
 
 ### DEFAULT BUILD MENU METHODS
+
+func meta_source_test(source : Dictionary,input):
+	var element = source.keys()[input]
+	var data = source.get(element).get("hp")
+	print("META RESULT: ",element," / ",data)
 
 func bus_test_one():
 	print("BUS TEST [ONE] SUCCESSFUL")
@@ -157,12 +188,3 @@ func create_campfire():
 func immediate_category():
 	#RUN ANY CODE HERE, PRINTS TO PROVE THAT IT WORKS
 	print("IMMEDIATE CATEGORY TEST SUCCESSFUL")
-	
-
-
-
-
-
-
-
-##
